dd3061cda2be2532945bbc16ee05b6fdea13cb17
CHAIMYY <houas.chaimaa@gmail.com>
2024-01-12 17:19:52 +0100

    brief wiki

---
+++ app/config/configuration.php
@@ -0,0 +1,8 @@
+<?php
+
+    /* Project Configuration */ 
+    define("WebsiteName", "wiki");
+    define("UrlRoot", "http://localhost/brief-wiki");
+    define("AppRoot", dirname(dirname(__FILE__)));
+
+?>
